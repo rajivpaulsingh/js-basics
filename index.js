d04684c4fc3175bcfaf282536b90c00b4ad33177
Rajiv Singh <rajivpaulsingh@gmail.com>
2020-03-19 15:59:30 -0700

    Added variables and constants

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 // This is my first JavaScript code
 console.log('Hello world');
+
+// Variables
+let name = 'Rajiv';
+console.log(name);
+
+let firstName = 'Rajiv';
+let lastName = 'Singh';
+console.log(firstName + ' ' + lastName);
+
+// Constants
+const interestRate = '3.0';
+console.log(interestRate);
+
+//Primitive types - String, Number, Boolean, Undefined, Null
+let name1 = 'Rajiv'; //String literal
+let age = '35'; //Number literal
+let isApproved = true; //Boolean literal
+let firstName1; //Undefined
+let middleName1 = undefined //Undefined
+let lastName1 = null; //Null
