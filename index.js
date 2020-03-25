f9b065bb058aacb5e8321ed8c340932181b6ffe6
Rajiv Singh <rajivpaulsingh@gmail.com>
2020-03-25 12:26:14 -0700

    Operators of javascript

--- index.js
+++ index.js
@@ -53,10 +53,73 @@ function greet(name3) { //parameter
 greet('Rajiv'); //argument
 greet('Singh');
 
-//Function - Calculating a value
+// Function - Calculating a value
 function square(number) {
     return number * number;
 }
 let number = square(2);
 console.log(number);
 console.log(square(2));
+
+// Operator types - Arithmetic, Assignment, Comparison, Logical, Bitwise
+let x = 10;
+let y = 3;
+
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+console.log(x ** y);
+
+// Increment and Decrement operators
+console.log(++x);
+console.log(--x);
+
+// Comparison operators
+console.log(x > 0);
+console.log(x >= 10);
+console.log(x < 0);
+console.log(x <= 0);
+console.log(x === 10);
+console.log(x !== 10);
+
+// Strict equality operator - both operant types are of same type and values
+console.log(1 === 1);
+console.log('1' === 1);
+
+// Lose equality operator - it converts the right value to match with right value 
+// and only checks if values are equal
+console.log(1 == 1);
+console.log('1' == 1);
+console.log(true == 1);
+
+// Ternary operators
+// If a customer has more than 100 points, they are 'gold' otherwise they are 'silver'
+let points = 110;
+let type = points > 100 ? 'gold' : 'silver';
+console.log(type);
+
+// Logical operators
+let highIncome = false;
+let goodCreditScore = false;
+let eligibleForLoan = highIncome || goodCreditScore;
+console.log('Eligible ', eligibleForLoan);
+
+let applicationRefused = !eligibleForLoan;
+console.log('Application Refused ', applicationRefused);
+
+// Bitwise operators
+// 1 = 00000001;
+// 2 = 00000010;
+// 3 = 00000011;
+// Result 1 | 2 = 00000011;
+// Result 1 % 2 = 00000000;
+console.log(1 | 2);
+console.log(1 & 2);
+
+// Read, Write and Execute permissions
+// 00000100
+// 00000110
+// 00000111
+
