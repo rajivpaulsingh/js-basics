e34f79211a748f49c3c7b9e2362fdff5df1dae71
Rajiv Singh <rajivpaulsingh@gmail.com>
2020-04-08 13:56:06 -0700

    Object equality

--- index.js
+++ index.js
@@ -295,3 +295,27 @@ function showAddress(address) {
 }
 showAddress(address);
 
+//Exercise - Are objects equal and same
+//Constructor Function
+function Address(street, city, zipCode) {
+    this.street = street;
+    this.city = city;
+    this.zipCode = zipCode;
+}
+
+let address1 = new Address('a', 'b', 'c');
+let address2 = new Address('a', 'b', 'c');
+
+console.log(areEqual(address1, address2));
+console.log(areSame(address1, address2));
+
+function areEqual(address1, address2) {
+    return address1.street === address2.street && 
+    address1.city === address2.city &&
+    address1.zipCode === address2.zipCode;
+}
+
+function areSame(address1, address2) {
+    return address1 === address2;
+}
+
